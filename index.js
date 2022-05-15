1861907741232149a43ec5056e93c090682d0044
Ashiq Legi <ashiqlegi@gmail.com>
2022-05-15 13:24:01 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+// Write your code here
+
+main.remove()
+
+const newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.textContent = "YOUR-NAME is the champion"
+
+element.append(newHeader)
+
